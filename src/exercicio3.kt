b2766ee2aa800b878fb970f3265e2fc0ad1261dc
kauanlusbel <lusbelk@gmail.com>
2024-09-03 17:45:22 -0300

    kotlin hora de codar3

--- src/exercicio3.kt
+++ src/exercicio3.kt
@@ -1,25 +1,9 @@
-/*Faça um programa para ler 3 valores (considere que não serão informados valores iguais) e escrever o maior deles.
+// - Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem decrescente.
+//Exemplo: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
 
- */
 fun main(){
-
-    var list = mutableListOf<Int>()
-    for (a in 1.. 3 ){
-        println("digite um numero: ")
-        list.add(readln().toInt())
-
+    for(i in 10 downTo 1){
+        println(i)
+        Thread.sleep(1000)
     }
-    if (list[0]> list[1] && list[0]> list[2]){
-        println("o maior numero é ${list[0]}")
-    }
-    else if (list[1] > list[2] && list[1]>list[0]){
-        println("o maior numero é${list[1]}")
-    }
-    else{
-        println("o maior numero é ${ list[2]}")
-    }
-
-
-
-
 }
